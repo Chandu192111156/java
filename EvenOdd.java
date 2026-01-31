09e1893614df1b68e1f29697c7564dcc7c89a42f
Chandu192111156 <112929177+Chandu192111156@users.noreply.github.com>
2026-01-31 23:37:45 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class EvenOdd 
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter a numbers");
+		int n=s.nextInt();
+		String res=n%2==0? n+"is Even":n+"is Odd";
+		System.out.println(res);
+	}
+}
